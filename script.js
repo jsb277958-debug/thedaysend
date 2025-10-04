a0efc0142734a991c14343f93fdcfb822f6e666d
jsb277958-debug <jsb277958@gmail.com>
2025-10-04 17:44:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+document.querySelectorAll('.deer').forEach(deer => {
+  deer.addEventListener('click', () => {
+    const clue = deer.dataset.clue;
+    window.location.href = clue; // redirects to hidden clue page
+  });
+});
